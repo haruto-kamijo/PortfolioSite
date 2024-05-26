a9a9e72bb048eac05d353df290878deecf001d8a
haruto-kamijo <kamijoharuto@gmail.com>
2024-05-26 21:28:03 +0900

    feat:all page initialize

---
+++ portfolio/src/app/Portfolio/page.tsx
@@ -0,0 +1,13 @@
+import Header from '@/app/Components/Header';
+import Footer from '@/app/Components/Footer';
+
+export default function Page() {
+	return (
+		<div className="flex h-screen p-0 bg-sky-100 seven-gradation">
+			<Header title="Portfolio Site"></Header>
+			<div className="flex overflow-x-auto h-5/6 absolute top-12 mt-3">
+			</div>
+			<Footer/>
+		</div>
+	);
+}
