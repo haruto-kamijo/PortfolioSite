d442698894a4e67c41717af99ef8f87af8dfa154
haruto-kamijo <kamijoharuto@gmail.com>
2024-06-04 23:31:22 +0900

    feat:Add text-box and shadow

--- portfolio/src/app/(Components)/FV.tsx
+++ portfolio/src/app/(Components)/FV.tsx
@@ -10,25 +10,47 @@ import React from 'react'
 const FV = () => {
 
 	return (
-		<section className="flex-none w-screen h-screen ">
-			{/*<div className="absolute z-0 mt-0 flex flex-wrap catchcopy-container">*/}
-			{/*	<p className=" catchcopy">Find</p>*/}
-			{/*	<p className="catchcopy">your</p>*/}
-			{/*	<p className="catchcopy">own</p>*/}
-			{/*	<p className="catchcopy">color</p>*/}
-			{/*</div>*/}
+		<section className="flex-none w-screen h-screen">
 			<div className="profile relative">
-				<h1 className="text-3xl z-10 lg:text-7xl bg-emerald-400 w-max">Haruto Kamijo</h1>
-				{/*<h1 className="text-3xl z-10 lg:text-7xl"></h1>*/}
-				<div className="relative text-3xl z-10 lg:text-5xl mt-16"><p className="bg-blue-500 w-max">Skills</p>
-					<h2 className="text-3xl z-10 lg:text-5xl mt-0 ">
-						<p className="mt-2 w-max bg-purple-400">Game:<span className="text-white hover:text-orange-300">Java/Android Studio,</span><span className="text-white hover:text-orange-300">C#/Unity</span></p>
-						<p className="mt-2 w-max bg-purple-400">Web:<span className="text-white hover:text-orange-300">HTML/CSS/JS,</span><span className="text-white hover:text-orange-300">TS/Next.js/Tailwind</span></p>
-						<p className="mt-2 w-max bg-purple-400">AI:<span className="text-white hover:text-orange-300">Python/PyTorch,</span><span className="text-white hover:text-orange-300">Python/TensorFlow/Keras</span></p>
-						<p className="mt-2 w-max bg-purple-400">Other:<span className="text-white hover:text-orange-300">3D Model/Blender,</span><span className="text-white hover:text-orange-300">Design/Illustrator/Photoshop</span></p>
-
+				<div>
+					<h1 className="relative text-3xl z-10 lg:text-7xl bg-emerald-400 w-max pb-2">Haruto Kamijo</h1>
+					{/*shadow*/}
+					<h1 className="text-3xl z-0 lg:text-7xl name-shadow absolute bg-black -top-2 left-3 w-max text-black pb-2 ">Haruto Kamiio</h1>
+					
+				</div>
+				<div className="relative text-3xl z-10 lg:text-5xl mt-12">
+					<p className="relative bg-blue-500 z-10 w-max">Skills</p>
+					{/*shadow*/}
+					<p className="absolute bg-black top-2 left-2 w-max text-black">Skills</p>
+					
+					<h2 className="relative text-3xl z-10 lg:text-5xl mt-7 ">
+						<p className="mt-4 w-max bg-purple-400 pb-2">Game:<span className="text-white hover:text-orange-300">Java/Android Studio,</span><span
+							className="text-white hover:text-orange-300">C#/Unity</span></p>
+						<p className="mt-3 w-max bg-purple-400 pb-2">Web:<span
+							className="text-white hover:text-orange-300">HTML/CSS/JS,</span><span
+							className="text-white hover:text-orange-300">TS/Next.js/Tailwind</span></p>
+						<p className="mt-3 w-max bg-purple-400 pb-2">AI:<span
+							className="text-white hover:text-orange-300">Python/PyTorch,</span><span
+							className="text-white hover:text-orange-300">Python/TensorFlow/Keras</span></p>
+						<p className="mt-3 w-max bg-purple-400 pb-2">Other:<span className="text-white hover:text-orange-300">3D Model/Blender,</span><span
+							className="text-white hover:text-orange-300">Design/Illustrator/Photoshop</span></p>
 					</h2>
-					{/*<h2 className="text-3xl z-10 lg:text-5xl"></h2>*/}
+					<div>{/*shadow*/}
+						<h2 className="absolute top-12 left-2 text-3xl z-0 lg:text-5xl mt-5 text-black">
+						<p className="mt-4 w-max bg-black pb-2">Game:<span className="text-black">Java/Android Studio,</span><span
+							className="text-black">C#/Unity</span></p>
+						<p className="mt-3 w-max bg-black pb-2">Web:<span
+							className="text-black">HTML/CSS/JS,</span><span
+							className="text-black">TS/Next.js/Tailwind</span></p>
+						<p className="mt-3 w-max bg-black pb-2">AI:<span
+							className="text-black">Python/PyTorch,</span><span
+							className="text-black0">Python/TensorFlow/Keras</span></p>
+						<p className="mt-3 w-max bg-black pb-2">Other:<span className="text-black">3D Model/Blender,</span><span
+							className="text-black">Design/Illustrator/Photoshop</span></p>
+					</h2>
+						
+					</div>
+
 				</div>
 			</div>
 
--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -4,7 +4,7 @@ import Footer from '@/app/(Components)/Footer';
 
 export default function Page() {
   return (
-    <div className="flex h-screen p-0 bg-sky-100 double-gradation">
+    <div className="flex h-screen p-0 double-gradation">
       <Header title="Kamijo Haruto"></Header>
       <FV></FV>
       <Footer/>
