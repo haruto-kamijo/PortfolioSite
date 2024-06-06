c0e10363a7f5b1e3c34f64dc6fdfe28e2d9079a1
haruto-kamijo <kamijoharuto@gmail.com>
2024-06-06 22:14:56 +0900

    feat:Add hover

--- portfolio/src/app/(Components)/FV.tsx
+++ portfolio/src/app/(Components)/FV.tsx
@@ -13,45 +13,29 @@ const FV = () => {
 		<section className="flex-none w-screen h-screen">
 			<div className="profile relative">
 				<div>
-					<h1 className="relative text-3xl z-10 lg:text-7xl bg-emerald-400 w-max pb-2">Haruto Kamijo</h1>
-					{/*shadow*/}
-					<h1 className="text-3xl z-0 lg:text-7xl name-shadow absolute bg-black -top-2 left-3 w-max text-black pb-2 ">Haruto Kamiio</h1>
-					
+					<h1 className="relative text-3xl z-10 lg:text-7xl bg-emerald-400 w-max pb-2 name-shadow-border">Haruto Kamijo</h1>
 				</div>
-				<div className="relative text-3xl z-10 lg:text-5xl mt-12">
-					<p className="relative bg-blue-500 z-10 w-max">Skills</p>
-					{/*shadow*/}
-					<p className="absolute bg-black top-2 left-2 w-max text-black">Skills</p>
-					
-					<h2 className="relative text-3xl z-10 lg:text-5xl mt-7 ">
-						<p className="mt-4 w-max bg-purple-400 pb-2">Game:<span className="text-white hover:text-orange-300">Java/Android Studio,</span><span
-							className="text-white hover:text-orange-300">C#/Unity</span></p>
-						<p className="mt-3 w-max bg-purple-400 pb-2">Web:<span
-							className="text-white hover:text-orange-300">HTML/CSS/JS,</span><span
-							className="text-white hover:text-orange-300">TS/Next.js/Tailwind</span></p>
-						<p className="mt-3 w-max bg-purple-400 pb-2">AI:<span
-							className="text-white hover:text-orange-300">Python/PyTorch,</span><span
-							className="text-white hover:text-orange-300">Python/TensorFlow/Keras</span></p>
-						<p className="mt-3 w-max bg-purple-400 pb-2">Other:<span className="text-white hover:text-orange-300">3D Model/Blender,</span><span
-							className="text-white hover:text-orange-300">Design/Illustrator/Photoshop</span></p>
+				<div className="relative text-3xl z-10 lg:text-5xl mt-10 ">
+					<p className="relative bg-blue-500 z-10 w-max skills-shadow-border">Skills</p>
+					<h2 className="relative text-3xl z-10 lg:text-5xl pt-5 ">
+						<p className="mt-3 w-max bg-purple-400 pb-2 skill-fcontents-shadow-border">Game:
+							<span className="text-white hover:text-orange-300">Java/Android Studio,</span>
+							<span className="text-white hover:text-orange-300">C#/Unity</span>
+						</p>
+						<p className="mt-4 w-max bg-purple-400 pb-2 skill-contents-shadow-border">Web:
+							<span className="text-white hover:text-orange-300">HTML/CSS/JS,</span>
+							<span className="text-white hover:text-orange-300">TS/Next.js/Tailwind</span>
+						</p>
+						<p className="mt-4 w-max bg-purple-400 pb-2 skill-contents-shadow-border">AI:
+							<span className="text-white hover:text-orange-300">Python/PyTorch,</span>
+							<span className="text-white hover:text-orange-300">Python/TensorFlow/Keras</span>
+						</p>
+						<p className="mt-4 w-max bg-purple-400 pb-2 skill-contents-shadow-border">Other:
+							<span className="text-white hover:text-orange-300">3D Model/Blender,</span>
+							<span className="text-white hover:text-orange-300">Design/Illustrator/Photoshop</span>
+						</p>
 					</h2>
-					<div>{/*shadow*/}
-						<h2 className="absolute top-12 left-2 text-3xl z-0 lg:text-5xl mt-5 text-black">
-						<p className="mt-4 w-max bg-black pb-2">Game:<span className="text-black">Java/Android Studio,</span><span
-							className="text-black">C#/Unity</span></p>
-						<p className="mt-3 w-max bg-black pb-2">Web:<span
-							className="text-black">HTML/CSS/JS,</span><span
-							className="text-black">TS/Next.js/Tailwind</span></p>
-						<p className="mt-3 w-max bg-black pb-2">AI:<span
-							className="text-black">Python/PyTorch,</span><span
-							className="text-black0">Python/TensorFlow/Keras</span></p>
-						<p className="mt-3 w-max bg-black pb-2">Other:<span className="text-black">3D Model/Blender,</span><span
-							className="text-black">Design/Illustrator/Photoshop</span></p>
-					</h2>
-						
 					</div>
-
-				</div>
 			</div>
 
 		</section>
