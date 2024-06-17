aa5d56bd488d9ecf4ef629c872b4dd814bdedf8c
haruto-kamijo <kamijoharuto@gmail.com>
2024-06-17 11:34:24 +0900

    feat:change text-size in index page

--- portfolio/src/app/(Components)/FV.tsx
+++ portfolio/src/app/(Components)/FV.tsx
@@ -15,9 +15,9 @@ const FV = () => {
 				<div>
 					<h1 className="relative text-3xl z-10 lg:text-7xl bg-emerald-400 w-max pb-2 name-shadow-border">Haruto Kamijo</h1>
 				</div>
-				<div className="relative text-3xl z-10 lg:text-5xl mt-10 ">
+				<div className="relative text-3xl z-10 lg:text-4xl mt-10 ">
 					<p className="relative bg-blue-500 z-10 w-max skills-shadow-border">Skills</p>
-					<h2 className="relative text-3xl z-10 lg:text-5xl pt-5 ">
+					<h2 className="relative text-3xl z-10 lg:text-4xl pt-5 ">
 						<p className="mt-3 w-max bg-purple-400 pb-2 skill-fcontents-shadow-border">Game:
 							<span className="text-white hover:text-orange-300">Java/Android Studio,</span>
 							<span className="text-white hover:text-orange-300">C#/Unity</span>
