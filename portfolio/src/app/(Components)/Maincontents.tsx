714758c23e7d1574b38ec4632ffc39c9d06fcd56
haruto-kamijo <kamijoharuto@gmail.com>
2024-06-18 21:27:21 +0900

    feat:add portfolio contents

---
+++ portfolio/src/app/(Components)/Maincontents.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Link from 'next/link'
+
+type MaincontentsProps = {
+	count:number;
+}
+
+const Maintcontents = ({ count}: MaincontentsProps) => {
+	
+	return (
+		<div className="main-contents">
+			<div className={count === 1 ? "contents-active" : "contents-nonactive"}>
+				<p className="contents-title">My Web Site</p>
+				<div className="absolute pc top-14 flex bg-sky-200">
+					<div className="frame mt-6">
+						<iframe className="absolute" width="1920" height="1080" src="https://digitalaquarium-1bea2.web.app/"/>
+					</div>
+					<div className="frame mt-6">
+						<iframe className="absolute" width="1920" height="1080" src="https://harugassyukuhp-8c700.web.app/"/>
+					</div>
+				</div>
+				<Link href="./Portfolio/Sites" className="contents-link">get more information</Link>
+			</div>
+			<div className={count === 2 ? "contents-active" : "contents-nonactive"}>
+				<p className="contents-title">My Game</p>
+				<p className="relative pt-32 text-center text-4xl">Coming soon....</p>
+				<Link href="./Portfolio/Games" className="contents-link">get more information</Link>
+			</div>
+			<div className={count === 3 ? "contents-active" : "contents-nonactive"}>
+				<p className="contents-title">My 3D Model</p>
+				<p className="relative pt-32 text-center text-4xl">Coming soon....</p>
+				<Link href="./Portfolio/Models" className="contents-link">get more information</Link>
+			</div>
+			<div className={count === 4 ? "contents-active" : "contents-nonactive"}>
+				<p className="contents-title">My Photo</p>
+				<p className="relative pt-32 text-center text-4xl">Coming soon....</p>
+				<Link href="./Portfolio/Photos" className="contents-link">get more information</Link>
+			</div>
+			<div className={count === 5 ? "contents-active" : "contents-nonactive"}>
+				<p className="contents-title">My Github</p>
+				<div className="github-link"><Link href="https://github.com/haruto-kamijo" rel="noopener noreferrer" target="_blank">go to my github page</Link></div>
+			</div>
+		</div>
+	)
+}
+
+export default Maintcontents;
--- portfolio/src/app/Portfolio/page.tsx
+++ portfolio/src/app/Portfolio/page.tsx
@@ -1,22 +1,48 @@
+"use client"
 import Header from '@/app/(Components)/Header';
 import Footer from '@/app/(Components)/Footer';
-import FV from "@/app/(Components)/FV";
+import {useState} from "react"
+import Maintcontents from "@/app/(Components)/Maincontents";
 
-export default function Page() {
+
+
+
+const Portfolio = () => {
+	const MaxContents=5;
+	const [count, setCount] = useState(0);
+
+	const AddCount=()=>{
+		if(count>=MaxContents){
+			setCount(1);
+		}
+		else {
+			setCount(count+1)
+		}
+	}
+	const SubCount=()=>{
+		if(count<=1){
+			setCount(MaxContents);
+		}
+		else {
+			setCount(count-1)
+		}
+	}
+
 	return (
 		<div className="flex h-screen p-0 double-gradation">
 			<Header title="Kamijo Haruto"></Header>
 			<div>
-				<div className="main-contents"></div>
+				<Maintcontents count={count}></Maintcontents>
 				<div className="icon-contents">
-					<button>
-						<svg className="h-16 w-16 text-purple-500 icon-scroll" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5"
+					<button onClick={() => SubCount()}>
+						<svg className="h-16 w-16 text-purple-500 icon-scroll" width="24" height="24" viewBox="0 0 24 24"
+								 strokeWidth="1.5"
 								 stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
 							<path stroke="none" d="M0 0h24v24H0z"/>
 							<path d="M18 15l-6-6l-6 6h12" transform="rotate(270 12 12)"/>
 						</svg>
 					</button>
-					<button className="icon-bg">
+					<button className={count === 1? "icon-bg-active" : "icon-bg"} onClick={() => setCount(1)}>
 						<svg className="h-16 w-16 text-purple-500" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5"
 								 stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
 							<path stroke="none" d="M0 0h24v24H0z"/>
@@ -27,7 +53,7 @@ export default function Page() {
 							<line x1="14" y1="20" x2="20" y2="20"/>
 						</svg>
 					</button>
-					<button className="icon-bg">
+					<button className={count === 2 ? "icon-bg-active" : "icon-bg"} onClick={() => setCount(2)}>
 						<svg className="h-16 w-16 text-purple-500" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5"
 								 stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
 							<path stroke="none" d="M0 0h24v24H0z"/>
@@ -37,7 +63,7 @@ export default function Page() {
 							<line x1="18" y1="13" x2="18" y2="13.01"/>
 						</svg>
 					</button>
-					<button className="icon-bg">
+					<button className={count === 3 ? "icon-bg-active" : "icon-bg"} onClick={() => setCount(3)}>
 						<svg className="h-16 w-16 text-purple-500" viewBox="0 0 24 24" fill="none" stroke="currentColor"
 								 strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
 							<path
@@ -46,21 +72,22 @@ export default function Page() {
 							<line x1="12" y1="22.08" x2="12" y2="12"/>
 						</svg>
 					</button>
-					<button className="icon-bg">
+					<button className={count === 4 ? "icon-bg-active" : "icon-bg"} onClick={() => setCount(4)}>
 						<svg className="h-16 w-16 text-purple-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 							<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"
 										d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"/>
 						</svg>
 					</button>
-					<button className="icon-bg">
+					<button className={count === 5 ? "icon-bg-active" : "icon-bg"} onClick={() => setCount(5)}>
 						<svg className="h-16 w-16 text-purple-500" viewBox="0 0 24 23" fill="none" stroke="currentColor"
 								 strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
 							<path
 								d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"/>
 						</svg>
 					</button>
-					<button>
-						<svg className="h-16 w-16 text-purple-500 icon-scroll" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5"
+					<button onClick={() => AddCount()}>
+						<svg className="h-16 w-16 text-purple-500 icon-scroll" width="24" height="24" viewBox="0 0 24 24"
+								 strokeWidth="1.5"
 								 stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
 							<path stroke="none" d="M0 0h24v24H0z"/>
 							<path d="M18 15l-6-6l-6 6h12" transform="rotate(90 12 12)"/>
@@ -72,3 +99,4 @@ export default function Page() {
 		</div>
 	);
 }
+export default Portfolio;
